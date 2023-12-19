bdb96a944458373f7d6bbb3bc0ca71178069972b
Kolbein <kolbeinfoldoy@gmail.com>
2023-12-19 23:50:16 +0100

    Better local auth state

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,6 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'shoppinglist/shopping_list_view.dart';
 import 'signinscreen/signinscreen.dart';
@@ -14,11 +15,6 @@ class MyApp extends StatelessWidget {
   final SettingsController settingsController;
 
   const MyApp({super.key, required this.settingsController});
-
-  Future<bool> _isUserLoggedIn() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('isLoggedIn') ?? false;
-  }
 
   Future<String?> _getShoppingListId() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -40,48 +36,45 @@ class MyApp extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: const [
-            Locale('en', ''), // English, no country code
+            Locale('en', ''),
           ],
           onGenerateTitle: (BuildContext context) =>
               AppLocalizations.of(context)!.appTitle,
           theme: ThemeData(),
           darkTheme: ThemeData.dark(),
-          themeMode: EasyDynamicTheme.of(context)
-              .themeMode, //settingsController.themeMode,
+          themeMode: EasyDynamicTheme.of(context).themeMode,
           onGenerateRoute: (RouteSettings routeSettings) {
             return MaterialPageRoute<void>(
               settings: routeSettings,
               builder: (BuildContext context) {
-                // Add your route handling logic here
-                // If none of your conditions are met, return a default widget
                 return Container();
               },
             );
           },
-          home: FutureBuilder<bool>(
-            future: _isUserLoggedIn(),
-            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          home: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
+                return CircularProgressIndicator();
               } else {
-                if (snapshot.data != null && snapshot.data!) {
+                if (snapshot.data != null) {
                   return FutureBuilder<String?>(
                     future: _getShoppingListId(),
                     builder: (BuildContext context,
                         AsyncSnapshot<String?> snapshot) {
                       if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const CircularProgressIndicator();
+                        return CircularProgressIndicator();
                       } else {
                         if (snapshot.data != null) {
                           return ShoppingListView(listId: snapshot.data!);
                         } else {
-                          return const ShoppingListCreationView();
+                          return ShoppingListCreationView();
                         }
                       }
                     },
                   );
                 } else {
-                  return const SignInScreen();
+                  return SignInScreen();
                 }
               }
             },
